097d0a9ca074344a420d3275db93b21f765e66b5
Nael Mahouton <setondji.mn@gmail.com>
2021-03-23 00:58:51 +0100

    added comment documentation to the code

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// console.log("hello");
 // localStorage.clear();
 let person = [
     {"name" : "Doriane", "id" : 1 },
@@ -6,25 +5,28 @@ let person = [
     {"name" : "David", "id" : 3 },
     {"name" : "Bickel", "id" : 4 },
     {"name" : "Gary", "id" : 5 }, 
-    {"name" : "Beni", "id" : 6 },
-    {"name" : "David", "id" : 3 },
-    {"name" : "Bickel", "id" : 4 },
-    {"name" : "Gary", "id" : 5 }, 
+    {"name" : "Beni", "id" : 6 }, 
     {"name" : "Chris", "id" : 7 }
 ]; 
 
 // localStorage.setItem('members', JSON.stringify(person));
-
 // let persons2 = JSON.parse(localStorage.getItem('members'));
+
+
+/**
+ * This function takes an array and shuffles its content
+ * It returns a new array with random positions
+ */
 
 const shuffleArray = (array) => {
     let done = array.length;
     let shuffled = [];
 
+
     while(done>0){
-        let random = Math.round(Math.random()*100);
-        if (random < array.length && !shuffled.includes(array[random])){
-            shuffled.push(array[random]);
+        let random = Math.round(Math.random()*100); //generates random number between 0 and 100
+        if (random < array.length && !shuffled.includes(array[random])){ //if the number is in the scope of our list and is not already in the table
+            shuffled.push(array[random]); 
             done-=1;
         };
     }
@@ -33,10 +35,12 @@ const shuffleArray = (array) => {
 
 
 const getCouples = (people, size) =>{
+    
     let members = shuffleArray(people);
     let couples = [];
-    let nCouples = (people.length % size) === 1 ? Math.floor(people.length/size) : Math.ceil(people.length/size); 
 
+    //if there's 1 person left, we reduce the number of couples so they can later join the last couple. 
+    let nCouples = (people.length % size) === 1 ? Math.floor(people.length/size) : Math.ceil(people.length/size); 
 
     let i = 0;
     while(i<nCouples){
@@ -44,6 +48,7 @@ const getCouples = (people, size) =>{
         i+=1;
     }
 
+    //if there's one person left, we add him to the last couple (so there's no singles)
     if((people.length % size) === 1) 
     couples[i-1].push(members[members.length-1]);
 
@@ -52,6 +57,6 @@ const getCouples = (people, size) =>{
 
 
 
-console.log(getCouples(person, 4));
+console.log(getCouples(person, 2));
 
 
