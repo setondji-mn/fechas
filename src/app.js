3f17e87d4c1bc3e98662f01ea1b5379a8c7ec7a6
Nael Mahouton <setondji.mn@gmail.com>
2021-03-24 02:45:30 +0100

    reviewed files structure
    I split the index.js file into two files, one containing methods and the other containing the functional logic. Both js files have been moved to a src directory and included in the index

---
+++ src/app.js
@@ -0,0 +1,54 @@
+localStorage.clear();
+const members = [
+    {"name" : "Doriane", "id" : 1 },
+    {"name" : "Maelle", "id" : 2 },
+    {"name" : "David", "id" : 3 },
+    {"name" : "Bickel", "id" : 4 },
+    {"name" : "Gary", "id" : 5 }, 
+    {"name" : "Beni", "id" : 6 }, 
+    {"name" : "Chris", "id" : 7 },
+    {"name" : "Anicet", "id" : 8 },
+    {"name" : "Nael", "id" : 9 }
+];
+
+const couplesSize = 3;
+const nOfDates = 4;
+let datesCount = 0;
+let datesHistory = [];
+
+localStorage.setItem('datesHistory', JSON.stringify(datesHistory));
+localStorage.setItem('datesCount', JSON.stringify(datesCount));
+
+
+// localStorage.setItem('members', JSON.stringify(person));
+// let persons2 = JSON.parse(localStorage.getItem('members'));
+
+
+const launchDating = () => {
+
+    let newDate = generateCouples(members, couplesSize); 
+
+    datesHistory.forEach( oldDate => {
+
+        while(!isValidDate(newDate, oldDate)){
+            newDate = generateCouples(members, couplesSize);
+        }
+
+    });
+
+    datesHistory.push(newDate);
+    localStorage.setItem('datesHistory', JSON.stringify(datesHistory));
+
+    datesCount+=1;
+    localStorage.setItem('datesCount', JSON.stringify(datesCount));
+}
+
+
+
+
+
+for (let i = 0; i < nOfDates; i++) {
+    launchDating();
+}
+
+console.log(localStorage);
--- src/function.js
+++ src/function.js
@@ -1,28 +1,3 @@
-localStorage.clear();
-const members = [
-    {"name" : "Doriane", "id" : 1 },
-    {"name" : "Maelle", "id" : 2 },
-    {"name" : "David", "id" : 3 },
-    {"name" : "Bickel", "id" : 4 },
-    {"name" : "Gary", "id" : 5 }, 
-    {"name" : "Beni", "id" : 6 }, 
-    {"name" : "Chris", "id" : 7 },
-    {"name" : "Anicet", "id" : 8 },
-    {"name" : "Nael", "id" : 9 }
-];
-
-const couplesSize = 3;
-const nOfDates = 4;
-let datesCount = 0;
-let datesHistory = [];
-
-localStorage.setItem('datesHistory', JSON.stringify(datesHistory));
-localStorage.setItem('datesCount', JSON.stringify(datesCount));
-
-
-// localStorage.setItem('members', JSON.stringify(person));
-// let persons2 = JSON.parse(localStorage.getItem('members'));
-
 
 /**
  * This function takes an array and shuffles its content
@@ -96,36 +71,3 @@ const isValidDate = (newDate, oldDate) => {
     });
     return !check;
 }
-
-
-
-
-
-
-
-const launchDating = () => {
-
-    let newDate = generateCouples(members, couplesSize); 
-
-    datesHistory.forEach( oldDate => {
-
-        while(!isValidDate(newDate, oldDate)){
-            newDate = generateCouples(members, couplesSize);
-        }
-
-    });
-
-    datesHistory.push(newDate);
-    localStorage.setItem('datesHistory', JSON.stringify(datesHistory));
-
-    datesCount+=1;
-    localStorage.setItem('datesCount', JSON.stringify(datesCount));
-}
-
-
-
-
-
-// for (let i = 0; i < 4; i++) {
-//     launchDating();
-// }
